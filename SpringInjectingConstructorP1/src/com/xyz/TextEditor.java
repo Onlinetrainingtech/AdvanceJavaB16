4d198eab28d4293cb8b9c08cd213e42a35024b3a
OnlineTrainer <mohamedtechdata@gmail.com>
2023-01-06 21:18:30 +0530

    06-01-23

---
+++ SpringInjectingConstructorP1/src/com/xyz/TextEditor.java
@@ -0,0 +1,20 @@
+package com.xyz;
+
+public class TextEditor
+{
+   private SpellChecker spellChecker;
+   
+   
+   public TextEditor(SpellChecker spellChecker)
+   {
+	   System.out.println("Inside texteditor method");
+	   this.spellChecker=spellChecker;
+   }
+
+
+   
+   public void spellCheck()
+   {
+	   spellChecker.checkSpelling();
+   }
+}
